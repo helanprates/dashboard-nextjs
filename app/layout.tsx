79dfe4dcbd9a01be10c42af2414751e5f8695b11
Helan Prates <helanprates1@gmail.com>
2025-02-14 10:19:35 -0300

    Segundo commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import '@/app/ui/global.css'
+import {inter, lusitana} from '@/app/ui/fonts'
+import '@/app/ui/home.modules.css';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="pt-br">
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
